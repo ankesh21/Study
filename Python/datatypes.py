d6db247f5d5bf7f5c6b1bb580e47f6236f61ed4f
ankesh21 <ankesh03@gmail.com>
2023-06-07 01:36:16 +0530

    update

---
+++ Python/datatypes.py
@@ -0,0 +1,16 @@
+# data types in python
+    #Numeric type -- int, float, complex
+    #Str
+    #Boolean
+    #Sequence type--list , tuple, range
+    #mapping type--dict
+    #set type --set frozenset
+    #binary type --bytes,bytearray,memoryview
+
+a = 12 #integer
+b = "Ankesh" 
+
+loc_list = ['mumbai','pune','gurgaon','delhi','noida']
+loc_pin_dict = {'mumbai':400001,'noida':201301,'pune':411045} 
+planets_tuple = ['Mercury','venus','mars','earth']
+print(loc_list[0:3])
